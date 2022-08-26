e65367f553d7ac97659e4efb1cc64c349a448c9b
Alumnos <Alumnos@LABC2PC10>
2022-08-26 10:18:48 -0600

    Agregar archivos de proyecto.

---
+++ programa 3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+
+namespace programa_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList lista = new ArrayList();
+            Console.WriteLine("cantidad: {0", lista.Count);
+            Console.WriteLine("capacidad: [0", lista.Capacity);
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                lista.Add("Dato" + i);
+
+            }
+            Console.WriteLine("cantidad: {0", lista.Count);
+            Console.WriteLine("capacidad: [0", lista.Capacity);
+            Console.WriteLine();
+
+            for (int i = 10, i < 20; i++) ;
+            {
+                lista.Add("dato + i");
+            }
+            Console.WriteLine("cantidad: {0", lista.Count);
+            Console.WriteLine("capacidad: [0", lista.Capacity);
+            Console.WriteLine();
+            foreach(string row in lista)
+            {
+                Console.WriteLine(row);
+            }
+            Console.WriteLine("========Eliminar rango=========");
+            lista.RemoveRange(4, 4);
+            lista.RemoveRange(10, 4);
+            foreach (string row in lista)
+            {
+                Console.WriteLine(row);
+            }
+
+            lista.Insert(11, "Elemento insertado");
+            foreach (string row in lista)
+            {
+                Console.WriteLine(row);
+            }
+
+            lista.Clear();
+            Console.WriteLine("cantidad: {0}", lista.Count);
+            Console.WriteLine("capacidad: {0]", lista.Capacity);
+
+
+        }
+    }
+}
